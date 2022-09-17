a04e18035481e4b671f3551788ca53b40bc32c93
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-09-17 05:08:58 -0300

    Aula 2 - Mobile Home Screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native'
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_700Bold, 
+  Inter_900Black
+} from '@expo-google-fonts/inter'
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Loading } from './src/components/Loading';
+import { Home } from "./src/screens/Home"
+import { Background } from './src/components/Background';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-
-      <Button title="Send 1"></Button>
-      <Button title="Send 2"></Button>
-      <Button title="Send 3"></Button>
-
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+      { fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+import backgroundImg from '../../assets/background-galaxy.png';
+import { styles } from './styles';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground source={backgroundImg} defaultSource={backgroundImg}  style={styles.container}>
+        {children}
+    </ImageBackground>
+  );
+}
