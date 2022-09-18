404bbc768c2bb07b5376ef98417e365b85a91b2b
LucasVieiraS <lucas.silva3759@etec.sp.gov.br>
2022-09-18 16:41:10 -0300

    new: added game page on mobile

--- mobile/src/@types/navigation.d.ts
+++ mobile/src/@types/navigation.d.ts
@@ -1,12 +1,14 @@
+export interface GameParams {
+    id: string;
+    title: string;
+    bannerUrl: string;
+};
+
 export declare global {
     namespace ReactNavigation {
         interface RootParamList {
             home: undefined;
-            game: {
-                id: string;
-                title: string;
-                bannerUrl: string;
-            };
+            game: GameParams;
         }
     }
 }
--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,25 +1,89 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View, Image, FlatList, Text } from 'react-native';
 
-import { useRoute } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import { Entypo } from '@expo/vector-icons'
+
+import { styles } from './styles';
+import { THEME } from '../../theme';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
 
 import { Background } from '../../components/Background';
-import { styles } from './styles';
+import { Heading } from '../../components/Heading';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 
-interface RouteParams {
-    id: string;
-    title: string;
-    bannerUrl: string;
-}
+import { GameParams } from '../../@types/navigation';
+
 
 export function Game() {
 
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const navigation = useNavigation();
   const route = useRoute();
-  const game = route.params;
-  console.log(game);
+  const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.1.8:3333/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setDuos(data))
+  }, [])
 
   return (
     <Background>
         <SafeAreaView style={styles.container}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={handleGoBack}>
+              <Entypo 
+                name="chevron-thin-left"
+                color={THEME.COLORS.CAPTION_300}
+                size={20}
+              />
+            </TouchableOpacity>
+  
+            <Image 
+              source={logoImg}
+              style={styles.logo}
+            />
+
+            <View style={styles.right} />
+          </View> 
+
+          <Image 
+            source={{ uri: game.bannerUrl }}
+            style={styles.cover}
+            resizeMode="cover"
+          />
+
+          <Heading 
+            title={game.title}
+            subtitle="Conecte-se e comece a jogar."
+          />
+
+          <FlatList 
+            data={duos}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <DuoCard 
+                data={item} 
+                onConnect={() => {}}
+              />
+            )}
+            horizontal
+            style={styles.containerList}
+            contentContainerStyle={[duos.length > 0 ? styles.contentList : styles.emptyListContent]}
+            showsHorizontalScrollIndicator={false}
+            ListEmptyComponent={() => (
+              <Text style={styles.emptyListText}>
+                Não há anúncios publicados ainda.
+              </Text>
+            )}
+          />
 
         </SafeAreaView>
     </Background>
